d7685538bfd17bbe482d2a979dd41ad65491381d
Noah Lett <noahblett96@gmail.com>
2022-11-02 19:06:03 +0000

    Feature 1 create-an-entry completion #1

--- js/main.js
+++ js/main.js
@@ -30,3 +30,53 @@ function handleSubmit(event) {
   data.entries.unshift(obj);
   data.nextEntryId++;
 }
+
+// DOM Tree Creation Function //
+
+// function renderPost(obj) {
+//   var $li = document.createElement('li');
+
+//   var $divPost = document.createElement('div');
+//   $divPost.setAttribute('class', 'post');
+//   $li.appendChild($divPost);
+
+//   var $divRow = document.createElement('div');
+//   $divRow.setAttribute('class', 'row');
+//   $divPost.appendChild($divRow);
+
+//   var $divColumnHalf1 = document.createElement('div');
+//   $divColumnHalf1.setAttribute('class', 'column-half');
+//   $divRow.appendChild($divColumnHalf1);
+
+//   var $imgPicture = document.createElement('img');
+//   $imgPicture.setAttribute('src', obj.photoUrl);
+//   $imgPicture.setAttribute('class', 'picture');
+//   $divColumnHalf1.appendChild($imgPicture);
+
+//   var $divColumnHalf2 = document.createElement('div');
+//   $divColumnHalf2.setAttribute('class', 'column-half');
+//   $divRow.appendChild($divColumnHalf2);
+
+//   var $divTopic = document.createElement('div');
+//   $divColumnHalf2.appendChild($divTopic);
+
+//   var $h2Topic = document.createElement('h2');
+//   $h2Topic.setAttribute('class', 'topic');
+//   $divTopic.appendChild($h2Topic);
+
+//   var $divContent1 = document.createElement('div');
+//   $divColumnHalf2.appendChild($divContent1);
+
+//   var $pContent1 = document.createElement('p');
+//   $pContent1.setAttribute('class', 'content');
+//   $divContent1.appendChild($pContent1);
+
+//   var $divContent2 = document.createElement('div');
+//   $divColumnHalf2.appendChild($divContent2);
+
+//   var $pContent2 = document.createElement('p');
+//   $pContent2.setAttribute('class', 'content');
+//   $divContent2.appendChild($pContent2);
+
+//   return $li;
+// }
